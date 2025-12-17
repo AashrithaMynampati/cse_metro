08e8d8992ac68fd3356ed8e2b2d806136b6672ee
AashrithaMynampati <aashrithamynampati@gmail.com>
2025-12-17 10:18:17 +0530

    Add files via upload

---
+++ metro.py
@@ -0,0 +1,70 @@
+import streamlit as st
+import qrcode
+from io import BytesIO
+import uuid
+from PIL import Image
+from gtts import gTTS
+import base64
+
+#------------------------
+#QR GENERATION FUNCTION
+#------------------------
+def generate_qr(data):
+    qr=qrcode,QRCode(version=1,box_size=10,border=4)
+    qr.add_data(data)
+    qr.make(fit=True)
+    img=qr.make_image(fill_color="black",back_color="white")
+    return img
+#STREAMLIT UI
+st.set_page_config(page_title="Metro Ticket Booking",page_icon="0")
+st.title("metro ticket booking system with qr code + Auto Voice")
+stations=["Ameerpet","Miyapur","LB Nagar","KPHB","JNTU"]
+name=st.text_input("Passenger Name")
+source=st.selectbox("Source Station",stations)
+destination=st.selectbox("Destination Station",stations)
+no_tickets=st.number_input("Number of tickets",min_value=1,value=1)
+price_per_ticket=30
+total_amount=no_tickets*price_per_ticket
+st.info("Total Amount:rs(total_amount)")
+
+#BOOKING BUTTONS
+
+if st.button("Book Ticket"):
+    if name.strip()=="":
+        st.error("please enter passenger name.")
+    elif source=destination:
+        st.error("source and destination cannot be same")
+    else:
+        #generate booking ID
+        booking_id=str(uuid.uuid4())[:8]
+
+        #qr code generation
+
+        qr_data=(
+            f"BookingID:(booking_id)
+
+#QR CODE GENERATION
+
+
+      qr_data=(
+             f"BookingID:(booking_id)\n"
+             f"Name:(name)\nFrom: {source}\nTo:{destination}\n Tickets:{no_ticket}
+                )
+        qr_img=generate_qr(qr_data)
+
+             buf=BytesIO()
+            qr_img.save(buf,format="PNG")
+            qr_bytes=buf_getvalue()
+
+        st.success("Ticket Booked Successfully!")
+
+        st.write("### Ticket Details ")
+        st.write(f"**Booking ID:** {booking_id}")
+        st.write(f"**Passenger:**{name}")
+        st.write(f"**From:**{source}")
+        st.write(f"**To:**{destination}")
+        st.write(f"Tickets:**{no_tickets}")
+        st.write(f"**Amount paid:** rs{total_Amount}")
+        st.image(qr_bytes, width=250)
+            
+            
